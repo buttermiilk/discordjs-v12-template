752ce2166bd855fe42ec518f84655dbd1d6421d6
TheFateAkira <53138512+TheFateAkira@users.noreply.github.com>
2020-09-20 11:46:54 +0700

    chore: fix cmd.js

--- commands/cmd.js
+++ commands/cmd.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const data = require('quick.db');
 const settings = require('../settings.json');
 
-exports.run = async(client, message, args) => {
+exports.run = async(client, message, args) => { //eslint-no-unused-vars
   
-  //->...
+  //add your code to run the command.
   
 }
 
